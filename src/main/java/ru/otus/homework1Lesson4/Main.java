680e212045e3c4b828f293ad447dbb9c21fb32a5
lsdvitamin <lsdvitamin7@gmail.com>
2024-09-13 12:37:50 +0300

    Created project with README.md

---
+++ src/main/java/ru/otus/homework1Lesson4/Main.java
@@ -0,0 +1,11 @@
+package ru.otus.homework1Lesson4;
+
+/**
+ * @author Sergei on 13.09.2024 10:47.
+ * @project Default (Template) Project
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
